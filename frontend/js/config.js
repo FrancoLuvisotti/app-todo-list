36273ad26bb5bc5628fa9ff1a9f6daf475e4eef6
FrancoLuvisotti <franco_luvi@hotmail.com>
2026-02-07 01:57:21 -0300

    .

--- frontend/js/config.js
+++ frontend/js/config.js
@@ -1,1 +1,4 @@
-window.API_URL = 'https://app-todo-list-backend.onrender.com';
+//window.API_URL = 'https://app-todo-list-backend.onrender.com';
+window.API_URL = location.hostname.includes('localhost')
+    ? 'http://localhost:5000'
+    : 'https://app-todo-list-backend.onrender.com';
